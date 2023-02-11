208280bca7a8e9e98d4f6b5fe5ee7b3f4cd0e0c8
FayyerDugu <fayyerdugu@gmail.com>
2023-02-10 20:56:42 -0800

    Site updated: 2023-02-10 20:56:40

--- js/copy.js
+++ js/copy.js
@@ -1,17 +1,26 @@
-document.addEventListener("copy", function () {
-  debounce(function () {
-    new Vue({
-      data: function () {
-        this.$notify({
-          title: "Copied to Clipboard!",
-          message: "🎉",
-          position: 'top-left',
-          offset: 50,
-          showClose: true,
-          type: "success",
-          duration: 5000
-        });
-      }
-    })
-  }, 300);
-})
+var button = document.getElementById("debounce");
+const debounce = (func, delay) => {
+  let debounceTimer
+  return function () {
+    const context = this
+    const args = arguments
+    clearTimeout(debounceTimer)
+    debounceTimer
+      = setTimeout(() => func.apply(context, args), delay)
+  }
+}
+document.addEventListener("copy", debounce((e) => {
+  new Vue({
+    data: function () {
+      this.$notify({
+        title: "复制成功!",
+        message: "📖 注意缩进",
+        position: "top-left",
+        offset: 50,
+        showClose: true,
+        type: "success",
+        duration: 5000,
+      });
+    },
+  });
+}, 500));
